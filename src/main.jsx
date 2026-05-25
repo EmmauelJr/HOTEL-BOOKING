7e0114fdfad08c5801172751ea5d258e7f959400
Emmanuel JR <emmanuelolorunbepelumi@gmail.com>
2026-05-25 11:54:58 -0700

    hot

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,9 +7,9 @@ import { ClerkProvider } from '@clerk/react'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+
 )
